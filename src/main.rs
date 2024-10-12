19cfccc09dc2e1580f487a51420c9e05cbf8bbd5
tompro <tomas.prochazka@apertia.cz>
2024-10-12 17:16:27 +0200

    init repository

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+#![no_main]
+#![no_std]
+
+use log::info;
+use uefi::boot;
+use uefi::prelude::*;
+
+#[entry]
+fn main() -> Status {
+    uefi::helpers::init().unwrap();
+
+    info!("Hello world!");
+    boot::stall(10_000_000);
+
+    Status::SUCCESS
+}
